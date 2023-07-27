7535150461dbc1dca66dbf379c3c7f6d4bf2d90e
GabbLps <jhosegabriel95@gmail.com>
2023-07-27 18:32:06 -0300

    feat: implementa repositories e ajuste
    feat: implementa repositories e ajuste

---
+++ src/main/java/com/gabriel/helpdesk/repository/PessoaRepository.java
@@ -0,0 +1,11 @@
+package com.gabriel.helpdesk.repository;
+
+import com.gabriel.helpdesk.domain.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
+
+
+}
