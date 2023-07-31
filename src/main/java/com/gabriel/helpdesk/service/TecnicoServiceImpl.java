1b2e9d6d7eb1640d4a23159acb156a87198a2b41
GabbrielLps <jhosegabriel95@gmail.com>
2023-07-31 10:32:58 -0300

    feat: implementa exception p objeto nao encontrado
    feat: implementa exception p objeto nao encontrado

--- src/main/java/com/gabriel/helpdesk/service/TecnicoServiceImpl.java
+++ src/main/java/com/gabriel/helpdesk/service/TecnicoServiceImpl.java
@@ -1,8 +1,10 @@
 package com.gabriel.helpdesk.service;
 
 import com.gabriel.helpdesk.domain.Tecnico;
+import com.gabriel.helpdesk.exception.ObjectNotFoundException;
 import com.gabriel.helpdesk.repository.TecnicoRepository;
 import com.gabriel.helpdesk.service.interfaces.TecnicoService;
+import org.hibernate.JDBCException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class TecnicoServiceImpl implements TecnicoService {
 
     @Override
     public Tecnico buscaPorId(Integer id) {
-        return repository.findById(id).orElse(null);
+        return repository.findById(id).orElseThrow(() ->
+                new ObjectNotFoundException("Tecnico não encontrado com o ID informado."));
     }
 }
